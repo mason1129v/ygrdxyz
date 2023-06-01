cb363f20c4ff187c2e6853ab881641b27502745a
PinRay22 <102412799+PinRay22@users.noreply.github.com>
2023-06-02 02:19:13 +0800

    Add files via upload

--- mywebsite_cssok/chat/consumers.py
+++ mywebsite_cssok/chat/consumers.py
@@ -1,11 +1,14 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+import random
+
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_group_name = 'test'
-
+        # self.random_name = self.get_random_name()
+        
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
@@ -17,11 +20,14 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']    
-
+        
+        print(message)
+        print("=========")
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type':'chat_message',
+                # 'randomName': self.random_name,
                 'message':message
             }
         )
@@ -32,5 +38,10 @@ class ChatConsumer(WebsocketConsumer):
 
         self.send(text_data=json.dumps({
             'type':'chat',
+            # 'randomName' : random_name,
             'message': message
         }))
+
+    def get_random_name(self):
+        names = ["PIN", "DORRIS", "HUSI", "MASON"]
+        return random.choice(names)
--- mywebsite_cssok/chat/views.py
+++ mywebsite_cssok/chat/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from datetime import datetime, timezone, timedelta
 from chat.models import *
-
+import random
 
 
 # Create your views here.
+
+names = ["PIN", "mason", "doris", "Hui"]
 
 def lobby(request):
     return render(request, 'chat/lobby.html')
 
 now = datetime.datetime.now()
 checknum = 0
+
 def index_view(request):
     return render(request, 'chat/index.html', locals())
 
@@ -20,18 +23,19 @@ def host_view(request):
     return render(request, 'chat/host.html' )
 
 def client_view(request):
-    # mess = messbrd.objects.all()
+    Member_id = 'PIN'
+    mess = messbrd.objects.all()
     return render(request, 'chat/client.html', locals())
 
 def seperate(request):
     global checknum
     checknum += 1 
-    # mess = messbrd.objects.all()
-    # context = {
-    #     'checknum' : checknum,
-    #     'mess' : mess,
-    # }
-    return render(request, 'host.html', context)
+    mess = messbrd.objects.all()
+    context = {
+        'checknum' : checknum,
+        'mess' : mess,
+    }
+    return render(request, 'chat/host.html', context)
 
 def delete(request):
     global checknum
@@ -39,22 +43,22 @@ def delete(request):
         checknum -= 1 
     else :
         checknum = 0 
-    # mess = messbrd.objects.all()
-    # context = {
-    #     'checknum' : checknum,
-    #     'mess' : mess,
-    # }
-    return render(request, 'host.html', context)
+    mess = messbrd.objects.all()
+    context = {
+        'checknum' : checknum,
+        'mess' : mess,
+    }
+    return render(request, 'chat/host.html', context)
 
-def sendmess(request):
-    cont = request.POST.get('cont', '')
-    newmess = messbrd( MID = 1, MCont = cont, MDATE = now)
-    newmess.save()
-    text = cont
-    keyword = "鞋子"
-    found = search_keyword(text, keyword)
-    print("Keyword found:", found) 
-    return HttpResponseRedirect('/client/')
+# def sendmess(request):
+#     cont = request.POST.get('cont', '')
+#     newmess = messbrd( MID = 1, MCont = cont, MDATE = now)
+#     newmess.save()
+#     text = cont
+#     keyword = "鞋子"
+#     found = search_keyword(text, keyword)
+#     print("Keyword found:", found) 
+#     return HttpResponseRedirect('/client/')
 
 def search_keyword(text, keyword):
     if keyword.lower() in text.lower():
