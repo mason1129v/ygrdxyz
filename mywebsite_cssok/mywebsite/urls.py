ccb6df4f4b2dff4671a7dadf7018de74ac4928b7
mason1129v <102214505+mason1129v@users.noreply.github.com>
2023-06-06 21:27:49 +0800

    Add files via upload

--- mywebsite_cssok/mywebsite/urls.py
+++ mywebsite_cssok/mywebsite/urls.py
@@ -8,7 +8,12 @@ urlpatterns = [
     path('index/', views.index_view),
     path('host/', views.host_view),
     path('client/', views.client_view),
+    path('client_sp/', views.sp_view),
     path('seperate/', views.seperate),
     path('delete/', views.delete),
-    path('sendmess/', views.sendmess),
+    path('stream/', views.stream_view),
+    path('videos/', views.videos_view),
+    path('login/', views.login_view),
+    path('signup/', views.signup_view),
+    # path('sendmess/', views.sendmess),
 ]
