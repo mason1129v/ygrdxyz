f8254c709cefbfd71d85e1b994defe99a04ccf48
mason1129v <102214505+mason1129v@users.noreply.github.com>
2023-06-06 21:26:11 +0800

    Add files via upload

--- mywebsite_cssok/chat/views.py
+++ mywebsite_cssok/chat/views.py
@@ -2,51 +2,85 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from datetime import datetime, timezone, timedelta
 from chat.models import *
-import random
+
+# import random
 
 
 # Create your views here.
 
 names = ["PIN", "mason", "doris", "Hui"]
 
+
 def lobby(request):
     return render(request, 'chat/lobby.html')
+
 
 now = datetime.datetime.now()
 checknum = 0
 
+
 def index_view(request):
     return render(request, 'chat/index.html', locals())
 
+
 def host_view(request):
+    return render(request, 'chat/host.html', locals())
+
+def videos_view(request):
+    return render(request, 'chat/videos.html', locals())
+
+def login_view(request):
+    return render(request, 'chat/login.html', locals())
+
+
+def signup_view(request):
+    return render(request, 'chat/signup.html', locals())
+
+def sp_view(request):
+    m = member.objects.get(MID=3)
+    Member_id = m.MAC
     mess = messbrd.objects.all()
-    return render(request, 'chat/host.html' )
+    keyword = request.GET.get('keyword')
+    h = keyword
+    productname = product.objects.get(PID=h)
+    print(h)
+    print(productname)
+    return render(request, 'chat/client_sp.html', locals())
+
 
 def client_view(request):
-    Member_id = 'PIN'
+    m = member.objects.get(MID=3)
+    Member_id = m.MAC
     mess = messbrd.objects.all()
     return render(request, 'chat/client.html', locals())
 
+
+def stream_view(request):
+    mess = messbrd.objects.all()
+    return render(request, 'chat/stream.html')
+
+
 def seperate(request):
     global checknum
-    checknum += 1 
+    checknum += 1
     mess = messbrd.objects.all()
     context = {
-        'checknum' : checknum,
-        'mess' : mess,
+        'checknum': checknum,
+        'mess': mess,
     }
     return render(request, 'chat/host.html', context)
 
+
 def delete(request):
     global checknum
-    if checknum > 0 :
-        checknum -= 1 
-    else :
-        checknum = 0 
+    if checknum > 0:
+        checknum -= 1
+    else:
+        checknum = 0
     mess = messbrd.objects.all()
     context = {
-        'checknum' : checknum,
-        'mess' : mess,
+        'checknum': checknum,
+        'mess': mess,
     }
     return render(request, 'chat/host.html', context)
 
@@ -57,8 +91,9 @@ def delete(request):
 #     text = cont
 #     keyword = "鞋子"
 #     found = search_keyword(text, keyword)
-#     print("Keyword found:", found) 
+#     print("Keyword found:", found)
 #     return HttpResponseRedirect('/client/')
+
 
 def search_keyword(text, keyword):
     if keyword.lower() in text.lower():
