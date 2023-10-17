160b88764a0b16c86163a0d6e75d297413069fd3
PinRay22 <102412799+PinRay22@users.noreply.github.com>
2023-10-17 08:13:53 +0800

    Add files via upload

--- unii/settings.py
+++ unii/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = "django-insecure-dpo_4oi!19)-&t4+#oukly1-!#t&uta8_gp@=zw%@(ozzbm@-l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CSRF_COOKIE_SECURE = True
 
 ROOT_URLCONF = "unii.urls"
 
@@ -110,9 +113,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+# STATIC_URL = "static/"
+STATIC_URL = '/static/'
 
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- unii/urls.py
+++ unii/urls.py
@@ -6,11 +6,13 @@ from uni import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.index_view),
     path('index/', views.index_view),
     path('host/', views.host_view),
     path('client/', views.client_view),
     path('seperate/', views.seperate),
     path('delete/', views.delete),
     path('sendmess/', views.sendmess),
+    path('chat_with_ai/', views.chat_with_ai),
     
 ]
